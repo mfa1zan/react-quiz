0dc97f4ba8a2d230e746381da8191e0affc9a505
Muhammad Faizan <164984334+mfa1zan@users.noreply.github.com>
2025-07-28 13:02:58 +0500

    Add files via upload

---
+++ src/components/Timer.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+
+function Timer({dispatch,secoundsRemaining}) {
+
+    const mins = Math.floor(secoundsRemaining / 60)
+    const seconds = secoundsRemaining % 60
+
+    useEffect(function (){
+        const id = setInterval(function(){
+            dispatch({type : "tick"})
+        },1000)
+        return ()=> clearInterval(id)
+    },[dispatch])
+    return (
+        <div className="timer" > 
+            {mins < 10 && "0"}{mins}:{seconds < 10 && "0"}{seconds}
+        </div>
+    )
+}
+
+export default Timer
